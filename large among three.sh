4eaf3a666a83810e747dd879248c62ac19847b65
Sirupa-Rakesh <siruparakeshrakhi@gmail.com>
2026-03-12 08:00:37 +0530

    actions

--- large among three.sh
+++ large among three.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-read a
-read b
-read c
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+read -p "Enter third number: " c
+
 
 if [ $a -gt $b ] && [ $a -gt $c ]; then
     echo " $a is greather"
-elif [ $b -gt $c ]
+elif [ $b -gt $c ]; then
     echo " $b is greather"
 else 
     echo "$c is greather"
